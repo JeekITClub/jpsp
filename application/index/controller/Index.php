06b924cecdb80c7c623e97cdb61fee9b912cca25
wto <qiushimao@hotmail.com>
2017-03-07 18:33:09 +0800

    Update Index.php
    Update /application/index/index/controller/Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,4 +8,23 @@ class Index
         $view=new View();
         return $view->fetch($id,$name,$datetime,$url)
     }
+
+    public function upload(){
+    // 获取表单上传文件 例如上传了001.jpg
+    $file = request()->file('file');
+    // 移动到框架应用根目录/public/uploads/ 目录下
+    $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+    if($info){
+        // 成功上传后 获取上传信息
+        // 输出 jpg
+        echo $info->getExtension();
+        // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+        echo $info->getSaveName();
+        // 输出 42a79759f284b767dfcb2a0197904287.jpg
+        echo $info->getFilename(); 
+    }else{
+        // 上传失败获取错误信息
+        echo $file->getError();
+    }
+}
 }
